24a4103f87d2496ccb2c99a6961480b40d4f400b
16boot <paint.paint@mail.com>
2015-06-01 03:47:11 -0300

    Update htmlGenCat.js

--- htmlGenCat.js
+++ htmlGenCat.js
@@ -19,11 +19,8 @@ function     _title(params){
         document.write("<title>" + params[0] + "</title>");
 }
 function     _menu(params){
-
     for (i=0;i<params.length;i++){
-
         document.write(params[i].replace(","," "));
-
     }
 }
 function     _top(params){
@@ -32,13 +29,11 @@ var imageTop = "<img src='x' width='ypx' height='zpx' />".replace("x",params[0])
                                                          .replace("z",params[2]);
         document.write(imageTop);
 }
-function    _post(params){
+function     _post(params){
     for (i=0;i<params.length;i++){
         //<textarea style="width: 1323px; height: 307px;"> HELLO !!! ;v  </textarea>
         var textArea = "<textarea style='width: 1323px; height: 307px;''> TEXT  </textarea>".replace("TEXT",params[i]);
-
         document.write(textArea);
-
     }
 }
 
@@ -56,7 +51,7 @@ var  post   = [
                 "HELLO !!! ;v "
                 ];
 var  title  = [
-    "Cat site ;)"
+                 "Cat site ;)"
                 ];
 
 _top(topimg);
